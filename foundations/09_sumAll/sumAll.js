edabbd8d0cbd3ba92e8d6989b64121c5555dca21
Ryan Victor <ryan.silva61@gmail.com>
2026-03-14 00:49:36 -0300

    Realização do nono exercicios de Javascript

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if(!Number.isInteger(min) || !Number.isInteger(max)) {
+        return "Error"
+    } 
 
+
+
+  return (max*(max+1) - (min-1)*min) / 2;
 };
 
 // Do not edit below this line
