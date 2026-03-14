1ebc1c8591c38d8a5da09cfcab02bd170101b718
Ryan Victor <ryan.silva61@gmail.com>
2026-03-14 00:33:01 -0300

    Realização do oitavo exercicios de Javascript

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+    return arr.filter(item => !theArgs.includes(item));
 };
 
 // Do not edit below this line
