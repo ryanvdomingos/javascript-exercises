e1ac612800ea4ae977629aa68fd4f516e5ddb72d
Ryan Victor <ryan.silva61@gmail.com>
2026-03-14 01:16:12 -0300

    Realização do decimo-primeiro exercicios de Javascript

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+    C = Math.round((F - 32) * (5 / 9));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+    F = Math.round(C * (9 / 5) + 32);
+
 };
 
 // Do not edit below this line
