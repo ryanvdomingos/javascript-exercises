11c7057b4938620e63babc6a6aa6ca3e6d1b0a61
Ryan Victor <ryan.silva61@gmail.com>
2026-03-14 00:17:39 -0300

    Realização do setimo exercicios de Javascript

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let arr = [];
+    for(let i = input.lenght ; i > 0; i--) {
+        arr.push(input[i - 1]);
+    }
+    return arr;
 };
 
 // Do not edit below this line
