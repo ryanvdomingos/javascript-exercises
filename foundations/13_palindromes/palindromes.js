a8f6ed8490b7c56daae524229ce2c96fdc8dfc31
Ryan Victor <ryan.silva61@gmail.com>
2026-03-20 12:24:55 -0300

    realizacao do decimo terceiro exercicio de javascript

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const len = Math.floor(str.length / 2);
+  for (var i = 0; i < len; i++)
+    if (str[i] !== str[str.length - i - 1])
+      return false;
+  return true;
 };
 
 // Do not edit below this line
