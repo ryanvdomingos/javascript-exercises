4a779d39eb56a473296cb86545d566481b907d30
ryan victor <ryan.silva61@gmail.com>
2026-04-10 11:37:59 -0300

    Realização do segundo e terceiro exercicio de recursao

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,22 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(data) {
+  if (Number.isInteger(data)) {
+    return 1;
+  }
+
+  if (typeof data === "object" && data !== null) {
+    let count = 0;
+
+    const values = Object.values(data);
+
+    for (const value of values) {
+      count += totalIntegers(value);
+    }
+    return count;
+  }
+  return 0;
 };
-  
+
+console.log(totalIntegers([[[5], 3], 0, 2, ['foo'], [], [4, [5, 6]]])); // 7
+console.log(totalIntegers({ a: 1, b: { a: [5, 10], b: 11 } })); // 4
 // Do not edit below this line
 module.exports = totalIntegers;
