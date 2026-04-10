6a9fb794f87e54a405706703d9a78c4ea25218a5
ryan victor <ryan.silva61@gmail.com>
2026-04-10 11:51:45 -0300

    Realização do quarto exercicio de recursao

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,23 @@
-const permutations = function() {
-  
+const permutations = function(nums) {
+  let result = [];
+
+    if (nums.length === 0) return [[]];
+    if (nums.length === 1) return [nums];
+
+    for (let i = 0; i < nums.length; i++) {
+        const currentNum = nums[i];
+        const remainingNums = nums.slice(0, i).concat(nums.slice(i + 1));
+
+        const remainingNumsPermuted = permutations(remainingNums);
+
+      for (let j = 0; j < remainingNumsPermuted.length; j++) {
+        const permutedArray = [currentNum].concat(remainingNumsPermuted[j]);
+        result.push(permutedArray);
+      }
+    }
+
+  return result;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
